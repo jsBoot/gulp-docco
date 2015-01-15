508b426cadd123ffba0ddfbb5583017da82626d6
Steve Lacy <me@slacy.me>
2015-01-15 16:09:45 -0700

    lint js

--- test/main.js
+++ test/main.js
@@ -1,43 +1,43 @@
-/*global describe, it*/
-"use strict";
+/*global describe, it, xit*/
+'use strict';
 
-var fs = require("fs"),
-  es = require("event-stream"),
-  should = require("should");
+var fs = require('fs'),
+  es = require('event-stream'),
+  should = require('should');
 
-require("mocha");
+require('mocha');
 
-delete require.cache[require.resolve("../")];
+delete require.cache[require.resolve('../')];
 
-var gutil = require("gulp-util"),
-  docco = require("../");
+var gutil = require('gulp-util'),
+  docco = require('../');
 
-describe("gulp-docco", function () {
+describe('gulp-docco', function () {
 
   var expectedFile = new gutil.File({
-    path: "test/expected/hello.txt",
-    cwd: "test/",
-    base: "test/expected",
-    contents: fs.readFileSync("test/expected/hello.txt")
+    path: 'test/expected/hello.txt',
+    cwd: 'test/',
+    base: 'test/expected',
+    contents: fs.readFileSync('test/expected/hello.txt')
   });
 
-  xit("should produce expected file via buffer", function (done) {
+  xit('should produce expected file via buffer', function (done) {
 
     var srcFile = new gutil.File({
-      path: "test/fixtures/docco.litcoffee",
-      cwd: "test/",
-      base: "test/fixtures",
-      contents: fs.readFileSync("test/fixtures/docco.litcoffee")
+      path: 'test/fixtures/docco.litcoffee',
+      cwd: 'test/',
+      base: 'test/fixtures',
+      contents: fs.readFileSync('test/fixtures/docco.litcoffee')
     });
 
     var stream = docco();
 
-    stream.on("error", function(err) {
+    stream.on('error', function(err) {
       should.exist(err);
       done(err);
     });
 
-    stream.on("data", function (newFile) {
+    stream.on('data', function (newFile) {
 
       should.exist(newFile);
       should.exist(newFile.contents);
@@ -50,24 +50,24 @@ describe("gulp-docco", function () {
     stream.end();
   });
 
-  it("should error on stream", function (done) {
+  it('should error on stream', function (done) {
 
     var srcFile = new gutil.File({
-      path: "test/fixtures/docco.litcoffee",
-      cwd: "test/",
-      base: "test/fixtures",
-      contents: fs.createReadStream("test/fixtures/docco.litcoffee")
+      path: 'test/fixtures/docco.litcoffee',
+      cwd: 'test/',
+      base: 'test/fixtures',
+      contents: fs.createReadStream('test/fixtures/docco.litcoffee')
     });
 
     var stream = docco();
 
-    stream.on("error", function(err) {
+    stream.on('error', function(err) {
       should.exist(err);
       done();
     });
 
-    stream.on("data", function (newFile) {
-      newFile.contents.pipe(es.wait(function(err, data) {
+    stream.on('data', function (newFile) {
+      newFile.contents.pipe(es.wait(function(err) {
         done(err);
       }));
     });
@@ -77,23 +77,23 @@ describe("gulp-docco", function () {
   });
 
   /*
-  it("should produce expected file via stream", function (done) {
+  it('should produce expected file via stream', function (done) {
 
     var srcFile = new gutil.File({
-      path: "test/fixtures/hello.txt",
-      cwd: "test/",
-      base: "test/fixtures",
-      contents: fs.createReadStream("test/fixtures/hello.txt")
+      path: 'test/fixtures/hello.txt',
+      cwd: 'test/',
+      base: 'test/fixtures',
+      contents: fs.createReadStream('test/fixtures/hello.txt')
     });
 
-    var stream = docco("World");
+    var stream = docco('World');
 
-    stream.on("error", function(err) {
+    stream.on('error', function(err) {
       should.exist(err);
       done();
     });
 
-    stream.on("data", function (newFile) {
+    stream.on('data', function (newFile) {
 
       should.exist(newFile);
       should.exist(newFile.contents);
